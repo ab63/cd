96c9f2cc54b88516df6a5a56e430589c7554698b
Anuj Bansal <bansalanuj9@gmail.com>
2020-11-08 15:55:51 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import  Card from './Card';
+import Colleges from './colleges.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="heading">Colleges in North India</h1>
+      <div className="container">
+        {Colleges["colleges"].map((college)=>(
+                <Card college={college}></Card>
+        ))}
+      </div>
     </div>
+      
   );
 }
 
